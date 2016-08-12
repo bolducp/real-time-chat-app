ed454ae37804aba494e46e558c60141cf8e24197
Paige Bolduc <paigebolduc@gmail.com>
2016-08-12 11:07:30 -0400

    Set up basic server and front end code in place.
    Flask server with one default route and web socket connection.
    Index html template with minimal front-end jQuery code.

---
+++ chat_server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_sockets import Sockets
+
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+connections = []
+
+@sockets.route("/chat")
+def echo_socket(web_socket):
+    global connections
+    connections.append(web_socket)
+
+    while not web_socket.closed:
+        message = web_socket.receive()
+        connections = [c for c in connections if not c.closed]
+        for socket in connections:
+            socket.send(message)
+
+
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
