9e4bb18a3ae51d9d5207db7b25c95b9a117f5c31
Paige Bolduc <paigebolduc@gmail.com>
2016-09-09 16:29:43 -0400

    Organizational refactoring.

--- client/static/main.js
+++ client/static/main.js
@@ -1,7 +1,7 @@
 $(document).ready(init);
 
 function init(){
-    clickHandler();
+    clickHandlers();
     setRandomBackgroundColor();
     getAllMessages();
     getAllUsernames();
@@ -20,6 +20,12 @@ var socket = new WebSocket(ws_scheme + location.host + "/chat");
 var uid = createUID();
 var allMessages = [];
 var usernames = {};
+
+function clickHandlers() {
+    $("#sendMessage").click(sendMessage);
+    $("#updateUsername").click(updateUsername);
+    $("#message_input").keyup(sendOnEnter);
+}
 
 socket.onopen = function (event) {
     console.log("Websocket open!");
@@ -55,53 +61,6 @@ function addMessageToChatScreen(message) {
 
 function getUsername(uid) {
     return usernames[uid] || uid;
-}
-
-function createUID() {
-    return s4() + s4() + s4() + s4() + s4() + s4();
-}
-
-function generateRandomUsername() {
-    var adjectives =  [   
-        "affable", "agreeable", "ambitious", "amicable", "amusing", "charming", "conscientious",
-        "considerate", "diligent", "dynamic", "exuberant", "gregarious", "imaginative", "inventive",
-        "optimistic", "persistent", "pioneering", "philosophical", "practical", "rational", "reliable", 
-        "resourceful", "sensible", "sincere", "sociable", "versatile", "warmhearted", "witty"
-    ];
-
-    var animals = [ 
-        "anaconda", "macaw", "butterfly", "crocodile", "dragonfly", "mongoose", "fox", "gecko", "armadillo", 
-        "hummingbird", "iguana", "katydid", "lemur", "leopard", "puma", "panda", "sloth", "sting-ray"
-    ];
-
-    return toTitleCase(adjectives[Math.floor(Math.random() * adjectives.length)] + " " + animals[Math.floor(Math.random() * animals.length)]);
-}
-
-function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-    );
-}
-
-function initializeRandomUsername(username) {
-    var data = JSON.stringify({
-        uid: uid,
-        username: username
-    });
-
-    socket.send(data);
-    $("#username").text(username);
-}
-
-function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16);
-}
-
-function clickHandler() {
-    $("#sendMessage").click(sendMessage);
-    $("#updateUsername").click(updateUsername);
-    $("#message_input").keyup(sendOnEnter);
 }
 
 function sendMessage() {
@@ -140,14 +99,6 @@ function isUserMessage(message) {
     return message["uid"] === uid;
 }
 
-function setRandomBackgroundColor() {
-    var colors = [
-        "LightSteelBlue", "LightSalmon", "LightYellow", "LightSeaGreen", "MistyRose", "PaleTurquoise", "PaleVioletRed", "Thistle", "Lavender", "DarkTurquoise", "DarkSeaGreen", "CadetBlue", "LightGoldenRodYellow", "LightCyan", "LightCoral"
-    ];
-    var randomIndex = Math.floor(Math.random() * colors.length);
-    $("body").css({ "background-color": colors[randomIndex]});
-}
-
 function getAllMessages() {
     $.ajax({
         url: "/messages",
@@ -181,4 +132,53 @@ function setUpChat(messages) {
     for (var i = 0; i < messages.length; i++) {
         addMessageToChatScreen(messages[i]);
     }
+}
+
+function createUID() {
+    return s4() + s4() + s4() + s4() + s4() + s4();
+}
+
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16);
+}
+
+function generateRandomUsername() {
+    var adjectives =  [   
+        "affable", "agreeable", "ambitious", "amicable", "amusing", "charming", "conscientious",
+        "considerate", "diligent", "dynamic", "exuberant", "gregarious", "imaginative", "inventive",
+        "optimistic", "persistent", "pioneering", "philosophical", "practical", "rational", "reliable", 
+        "resourceful", "sensible", "sincere", "sociable", "versatile", "warmhearted", "witty"
+    ];
+
+    var animals = [ 
+        "anaconda", "macaw", "butterfly", "crocodile", "dragonfly", "mongoose", "fox", "gecko", "armadillo", 
+        "hummingbird", "iguana", "katydid", "lemur", "leopard", "puma", "panda", "sloth", "sting-ray"
+    ];
+
+    return toTitleCase(adjectives[Math.floor(Math.random() * adjectives.length)] + " " + animals[Math.floor(Math.random() * animals.length)]);
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+function initializeRandomUsername(username) {
+    var data = JSON.stringify({
+        uid: uid,
+        username: username
+    });
+
+    socket.send(data);
+    $("#username").text(username);
+}
+
+function setRandomBackgroundColor() {
+    var colors = [
+        "LightSteelBlue", "LightSalmon", "LightYellow", "LightSeaGreen", "MistyRose", "PaleTurquoise", "PaleVioletRed", "Thistle", "Lavender", "DarkTurquoise", "DarkSeaGreen", "CadetBlue", "LightGoldenRodYellow", "LightCyan", "LightCoral"
+    ];
+    var randomIndex = Math.floor(Math.random() * colors.length);
+    $("body").css({ "background-color": colors[randomIndex]});
 }
